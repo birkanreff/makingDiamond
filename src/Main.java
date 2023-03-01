548d2545d745022a4ba45e9bdfea100fa456a187
arwen <arwen@localhost>
2023-03-01 23:13:49 +0300

    www.patika.dev

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+	
+	public static void main(String [] args) {
+		
+		Car audi = new Car( "audi" ,  10 , "blue");
+		
+		audi.printInfo();
+		
+		
+		
+		
+		/*
+		
+		Car bmw = new Car();
+		
+		bmw.model = "BMW ";
+		bmw.speed = 20;
+		System.out.println(bmw.model + "Hızı: " + bmw.speed);
+		
+		
+		
+		
+		
+		
+		Car mercedes = new Car();
+		
+		*/
+	}
+
+}
